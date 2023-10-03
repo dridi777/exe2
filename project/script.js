8c7a80db2e120cfc4d57d7724b6de2633034e0a9
dridi777 <140327191+dridi777@users.noreply.github.com>
2023-10-03 16:03:57 +0200

    Add files via upload

---
+++ project/script.js
@@ -0,0 +1,8 @@
+let img = document.querySelector('.taswira');
+let container = document.querySelector('.container')
+function talifoun(phone){
+    img.src = phone;
+}
+function colors(alwen){
+    container.style.background = alwen;
+}
